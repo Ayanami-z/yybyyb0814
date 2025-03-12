77f0bd6d64243697d65dde52e91aa27fb18aafbe
Ayanami-z <2799242259@qq.com>
2025-03-12 18:01:00 +0800

    Add files via upload

---
+++ gamestore.sql
@@ -0,0 +1,80 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.31 : Database - game store
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`game store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `game store`;
+
+/*Table structure for table `bag` */
+
+DROP TABLE IF EXISTS `bag`;
+
+CREATE TABLE `bag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `propname` varchar(20) DEFAULT NULL,
+  `num` smallint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `bag` */
+
+/*Table structure for table `cart` */
+
+DROP TABLE IF EXISTS `cart`;
+
+CREATE TABLE `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `propname` varchar(20) DEFAULT NULL,
+  `num` smallint DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `cart` */
+
+/*Table structure for table `prop` */
+
+DROP TABLE IF EXISTS `prop`;
+
+CREATE TABLE `prop` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `propname` varchar(20) DEFAULT NULL,
+  `price` smallint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `prop` */
+
+insert  into `prop`(`id`,`propname`,`price`) values (1,'痛苦面具',2000),(2,'破军',2700),(3,'苍穹',2100),(4,'复活甲',2100),(5,'电刀',2200),(6,'破晓',3300),(7,'辉月',2100),(8,'名刀',1900);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `pw` int NOT NULL,
+  `money` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`pw`,`money`) values (1,NULL,0,0),(2,'1234',1234,0),(3,'4444',4444,0),(4,'8888',8888,16000),(5,'9999',9999,10000),(6,'12345',12345,0),(7,'1111',1111,0),(8,'2222',2222,0),(9,'3333',3333,0),(10,'9999',9999,10000),(11,'0',0,1010);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
